784493ad188b50de4301f5068b44b4ef1569bbc0
HAttori321 <berezaoksana1@gmail.com>
2024-03-03 13:12:04 +0200

    Add project files.

---
+++ Source12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "O Rose thou art sick.\n"
+        "The invisible worm,\n"
+        "That flies in the night\n"
+        "In the howling storm:\n"
+        "Has found out thy bed\n"
+        "Of crimson joy:\n"
+        "And his dark secret love\n"
+        "Does thy life destroy." << endl;
+    cout << endl;
+    cout << "William Blake"<< endl;
+
+    //Task2
+    cout << "\t\t\t\t\t\tEvery hunter wants to know\n"
+        "\t\t\t\t\tWhere the pheasant's from\n"
+        "\t\t\t\tHe aims for its tail, let it fly\n"
+        "\t\t\tAll for the sake of his pride\n"
+        "\t\tWho will be left to hear its call\n"
+        "\tIf none of the hunters will be here at all?" << endl;
+    //task3
+    cout << "Prodam cycenat"<<endl;
+    cout << "Poroda:rotveiler" << endl;
+    cout << endl;
+    cout << "..." <<endl;
+    cout << endl;
+    cout << "Tel:222-22-22" << endl;
+    cout << "222     | 222 |"<<endl;
+    cout << "22     | 22 |"<<endl;
+    cout << "2     | 2 |"<<endl;
+        
+
+}
